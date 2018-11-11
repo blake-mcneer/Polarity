c1c54dd7eebd1254005c155cb142e11daf23ea95
Blake McNeer <blake.mcneer@gmail.com>
2018-11-11 12:02:04 -0600

    Created initial classes to be used in level editing

---
+++ Assets/Scripts/EditorTool.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorTool : MonoBehaviour {
+
+    public GameObject GameManager;
+    public GameObject LevelEditor;
+    public enum PlayMode
+    {
+        Play,Edit
+    }
+    private void Start()
+    {
+        SetMode(PlayMode.Edit);
+    }
+    public void SetMode(PlayMode mode)
+    {
+        if (mode == PlayMode.Play)
+        {
+            GameManager.SetActive(true);
+            LevelEditor.SetActive(false);
+        }
+        else
+        {
+            GameManager.SetActive(false);
+            LevelEditor.SetActive(true);
+        }
+    }
+}
