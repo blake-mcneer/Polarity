3920b2097cc2983e6db51d21c8a1e7364608bc39
Blake McNeer <blake.mcneer@gmail.com>
2018-12-04 18:14:21 -0600

    Added level wipe option to recreate levels because of new coordinate system

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -24,9 +24,19 @@ public class MainMenu : MonoBehaviour {
     public void ConvertCoordinates()
     {
         for (int i = 1; i <= 30; i++){
-            UpdateLevel(i);
+            CreateNewSave(i);
         }
     }
+    public void CreateNewSave(int level)
+    {
+        string filename = "/Level" + level.ToString() + ".save";
+        BinaryFormatter newBF = new BinaryFormatter();
+        FileStream newFile = File.Create(Application.persistentDataPath + filename);
+        Save newSave = new Save();
+        newBF.Serialize(newFile, newSave);
+        newFile.Close();            
+    }
+
     //Delete all of this after kelly does his conversions
     public void UpdateLevel(int level = 0)
     {        
@@ -38,32 +48,35 @@ public class MainMenu : MonoBehaviour {
 
             Save oldSave = (Save)bf.Deserialize(file);
             file.Close();
+
             Save newSave = new Save();
-
-
             foreach (MagneticObjectData obj in oldSave.magneticObjectElements)
             {
                 Vector3 oldCoord = new Vector3(obj.posX, obj.posY, obj.posZ);
-                Vector3 newCoord = Camera.main.WorldToScreenPoint(oldCoord);
+                Vector3 newCoord = Camera.main.ScreenToWorldPoint(oldCoord);
+                Debug.Log("Converting magnetic obj from: " + oldCoord + " to: " + newCoord);
                 newSave.magneticObjectElements.Add(new MagneticObjectData(newCoord, obj.type, obj.index));
             }
             foreach (GoalData g in oldSave.goalElements)
             {
                 Vector3 oldCoord = new Vector3(g.posX, g.posY, g.posZ);
-                Vector3 newCoord = Camera.main.WorldToScreenPoint(oldCoord);
+                Vector3 newCoord = Camera.main.ScreenToWorldPoint(oldCoord);
                 newSave.goalElements.Add(new GoalData(newCoord, g.type, g.barrier, g.rotationSpeed));
 
             }
             foreach (AttractorData a in oldSave.attractorElements)
             {
                 Vector3 oldCoord = new Vector3(a.posX, a.posY, a.posZ);
-                Vector3 newCoord = Camera.main.WorldToScreenPoint(oldCoord);
+                Vector3 newCoord = Camera.main.ScreenToWorldPoint(oldCoord);
                 Quaternion newRot = new Quaternion(a.rotX, a.rotY, a.rotZ, a.rotW);
                 newSave.attractorElements.Add(new AttractorData(newCoord, newRot, a.strength));
             }
 
-            bf.Serialize(file, newSave);
-            file.Close();
+            BinaryFormatter newBF = new BinaryFormatter();
+            FileStream newFile = File.Create(Application.persistentDataPath + filename);
+            //Save save = (Save)bf.Deserialize(file);
+            newBF.Serialize(newFile, newSave);
+            newFile.Close();            
 
         }
         else
