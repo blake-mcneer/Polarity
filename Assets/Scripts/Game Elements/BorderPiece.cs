65d8bc2fa47432ef054819c2e1d2de3d4e4fa26d
Blake McNeer <blake.mcneer@gmail.com>
2018-12-15 14:09:24 -0600

    Fixed border rotations, off by 45 before

--- Assets/Scripts/Game Elements/BorderPiece.cs
+++ Assets/Scripts/Game Elements/BorderPiece.cs
@@ -31,25 +31,25 @@ public class BorderPiece : MonoBehaviour {
         switch (rot)
         {
             case RotationDegrees.None:
-                amt = 45.0f;
+                amt = 0.0f;
                 break;
             case RotationDegrees.S1_FortyFive:
-                amt = 90.0f;
+                amt = 45.0f;
                 break;
             case RotationDegrees.S2_Ninety:
-                amt = 135.0f;
+                amt = 90.0f;
                 break;
             case RotationDegrees.S3_OneThirtyFive:
-                amt = 180.0f;
+                amt = 135.0f;
                 break;
             case RotationDegrees.S4_OneEighty:
-                amt = 225.0f;
+                amt = 180.0f;
                 break;
             case RotationDegrees.S5_TwoTwentyFive:
-                amt = 270.0f;
+                amt = 225.0f;
                 break;
             case RotationDegrees.S6_TwoSeventy:
-                amt = 315.0f;
+                amt = 270.0f;
                 break;
             default:
                 amt = 0.0f;
