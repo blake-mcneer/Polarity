8d42cc18bd56151bd0301988fe8df1a8ffc99fdc
Blake McNeer <blake.mcneer@gmail.com>
2018-12-30 17:13:13 -0600

    Moved custom files under megabit folder

---
+++ Assets/Megabit/Scripts/Game Elements/BorderPiece.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorderPiece : MonoBehaviour {
+    public BorderPieceType type = BorderPieceType.BorderPieceStraight;
+    public RotationDegrees rot = RotationDegrees.None;
+    public GameObject cornerPrefab;
+    public GameObject straightPrefab;
+
+    private void Start()
+    {
+        ForceUpdate();
+    }
+    void ActivateSelectedPrefab()
+    {
+        switch (type){
+            case BorderPieceType.BorderPieceStraight:
+                cornerPrefab.SetActive(false);
+                straightPrefab.SetActive(true);
+                break;
+            case BorderPieceType.BorderPieceCorner:
+                straightPrefab.SetActive(false);
+                cornerPrefab.SetActive(true);
+                break;
+        }
+    }
+    void SetRotation()
+    {
+        float amt = 0.0f;
+        switch (rot)
+        {
+            case RotationDegrees.None:
+                amt = 0.0f;
+                break;
+            case RotationDegrees.S1_FortyFive:
+                amt = 45.0f;
+                break;
+            case RotationDegrees.S2_Ninety:
+                amt = 90.0f;
+                break;
+            case RotationDegrees.S3_OneThirtyFive:
+                amt = 135.0f;
+                break;
+            case RotationDegrees.S4_OneEighty:
+                amt = 180.0f;
+                break;
+            case RotationDegrees.S5_TwoTwentyFive:
+                amt = 225.0f;
+                break;
+            case RotationDegrees.S6_TwoSeventy:
+                amt = 270.0f;
+                break;
+            default:
+                amt = 0.0f;
+                break;
+        }
+        Vector3 rotVec3 = new Vector3(0.0f, 0.0f, amt);
+        Quaternion rotQuat = Quaternion.Euler(rotVec3);
+        transform.rotation = rotQuat;
+    }
+    public void ForceUpdate()
+    {
+        ActivateSelectedPrefab();
+        SetRotation();
+    }
+    private void OnDrawGizmosSelected()
+    {
+        ForceUpdate();
+    }
+}
---
+++ Assets/Megabit/Scripts/LevelEditor/GeneralEditor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralEditor : MonoBehaviour {
+
+    public GameObject BallPrefab;
+    public GameObject AttractorPrefab;
+    public GameObject GoalPrefab;
+    public GameObject BorderPiecePrefab;
+    Vector3 spawnPosition = new Vector3(6.0f, 0.0f, 0.0f);
+
+    EditorTool editorTool;
+    private void Start()
+    {
+        editorTool = FindObjectOfType<EditorTool>();
+    }
+    public void CreateBall()
+    {
+        Instantiate(BallPrefab,spawnPosition, Quaternion.Euler(0.0f,0.0f,0.0f));   
+    }
+    public void CreateAttractor()
+    {
+        Instantiate(AttractorPrefab,spawnPosition, Quaternion.Euler(0.0f,0.0f,0.0f));   
+    }
+    public void CreateGoal()
+    {
+        Instantiate(GoalPrefab,spawnPosition, Quaternion.Euler(0.0f,0.0f,0.0f));   
+    }
+    public void CreateBorderPiece()
+    {
+        Instantiate(BorderPiecePrefab, spawnPosition, Quaternion.Euler(0.0f, 0.0f, 0.0f));
+    }
+    public void TestMode()
+    {
+        editorTool.ToggleEditMode();
+        gameObject.SetActive(false);
+    }
+
+
+}
