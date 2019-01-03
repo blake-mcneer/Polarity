c488ffc4134fe082daf8505d3ffe6be6bd93198c
Kelly McNeer <kellymcneer@Kellys-MacBook-Pro.local>
2019-01-02 18:46:07 -0600

    Added fade variable to battle transitions.

--- Assets/BattleTransitions/SimpleBlit.cs
+++ Assets/BattleTransitions/SimpleBlit.cs
@@ -5,10 +5,17 @@ using System.Collections;
 public class SimpleBlit : MonoBehaviour
 {
     public Material TransitionMaterial;
+    public float cutoffVal = 0.0f;
+    public float fadeVal = 0.0f;
 
     void OnRenderImage(RenderTexture src, RenderTexture dst)
     {
         if (TransitionMaterial != null)
             Graphics.Blit(src, dst, TransitionMaterial);
+    }
+    private void Update()
+    {
+        TransitionMaterial.SetFloat("_Cutoff", cutoffVal);
+        TransitionMaterial.SetFloat("_Fade", fadeVal);
     }
 }
